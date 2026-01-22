f266f1df41fa382dd338ebf5eef921b2b25bb3f7
Umair Ashraf <36963276+Umair777@users.noreply.github.com>
2026-01-22 11:30:23 +0530

    update the break and continue

--- Python/Loops/break_continue.py
+++ Python/Loops/break_continue.py
@@ -11,3 +11,20 @@ for num in range(1, 10):
 3
 4
 Breaking the loop at: 5'''
+
+count = 0
+while count < 10:
+    count += 1
+    if count == 3:
+        continue  # skip printing 3
+    if count == 8:
+        break     # stop the loop when count is 8
+    print(count)
+    
+'''OUTPUT:
+1
+2
+4
+5
+6
+7'''
