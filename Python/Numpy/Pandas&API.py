4b00f6edd18a00aac8c05c99e6b70d224f3c3e6b
Umair Ashraf <36963276+Umair777@users.noreply.github.com>
2026-01-30 00:00:48 +0530

    update the nba

---
+++ Python/Numpy/Pandas&API.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from nba_api.stats.static import teams
+
+dict_={'a':[11,21,31],'b':[12,22,32]}
+
+df=pd.DataFrame(dict_)
+
+def one_dict(list_dict):
+    print('==========')
+    keys=list_dict[0].keys()
+    print('Print Keys:')
+    print(keys)
+    out_dict={key:[] for key in keys}
+    print('Initialized output dictionary:')
+    print(out_dict)
+    for dict_ in list_dict:
+        for key, value in dict_.items():
+            out_dict[key].append(value)
+    return out_dict
+
+nba_teams = teams.get_teams()
+threenba_teams = nba_teams[0:3]
+print(threenba_teams)
+print(type(threenba_teams))
+dict_nba_team=one_dict(threenba_teams)
+print(type(dict_nba_team))
+df_teams=pd.DataFrame(dict_nba_team)
+print("NBA Teams DataFrame:")
+print(df_teams.head())
+
+
